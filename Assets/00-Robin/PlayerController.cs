fdcd590c59c517a3348f8d9865aafb588238ec04
Bakakemono <32707953+Bakakemono@users.noreply.github.com>
2026-07-03 22:32:53 +0200

    Add Player Bobbing

--- Assets/00-Robin/PlayerController.cs
+++ Assets/00-Robin/PlayerController.cs
@@ -3,11 +3,32 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
     [SerializeField] Transform _playerVisual;
 
-    [SerializeField, Range(0f, 5f)]float _speed = 1f;
+    [SerializeField, Range(0f, 5f)] float _speed = 1f;
+    [SerializeField, Range(0f, 5f)] float _bobbingSpeed = 1f;
+    [SerializeField] float _bobbingAngle = 25f;
+    float _currentAngle = 0f;
+    float _angleModifier = 1f;
 
     void Update() {
         Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
 
         transform.Translate(input * _speed * Time.deltaTime);
+
+        if(input.sqrMagnitude > 0f) {
+            if(_currentAngle == _bobbingAngle * _angleModifier) {
+                _angleModifier *= -1f;
+            }
+            else {
+                _currentAngle = Mathf.Lerp(_currentAngle, _bobbingAngle * _angleModifier, 0.1f);
+                if(Mathf.Abs(_currentAngle - _bobbingAngle * _angleModifier) < 0.1f) {
+                    _currentAngle = _bobbingAngle * _angleModifier;
+                }
+                _playerVisual.rotation = Quaternion.Euler(0f, 0f, _currentAngle);
+            }
+        }
+        else {
+            _playerVisual.rotation = Quaternion.Lerp(_playerVisual.rotation, Quaternion.identity, 0.1f);
+            _currentAngle = 0f;
+        }
     }
 }
